260ce6d4cb3e00ea25face0b02715dc12e3ca2db
Alex Carol <acarolcampre@adobe.com>
2026-03-10 17:49:11 +0100

    Revert "Second attempt at component generation"
    This reverts commit 6574c65c66322d5e66d5c0811966c2d8e1d20abc.

--- .claude/skills/ue-enable/references/examples/scripts-integration.js
+++
@@ -1,5 +0,0 @@
-// UE Editor support before page load
-if (/\.(stage-ue|ue)\.da\.live$/.test(window.location.hostname)) {
-  // eslint-disable-next-line import/no-unresolved
-  await import(`${window.hlx.codeBasePath}/ue/scripts/ue.js`).then(({ default: ue }) => ue());
-}
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -186,10 +186,4 @@ async function loadPage() {
   loadDelayed();
 }
 
-// UE Editor support before page load
-if (/\.(stage-ue|ue)\.da\.live$/.test(window.location.hostname)) {
-  // eslint-disable-next-line import/no-unresolved
-  await import(`${window.hlx.codeBasePath}/ue/scripts/ue.js`).then(({ default: ue }) => ue());
-}
-
 loadPage();
